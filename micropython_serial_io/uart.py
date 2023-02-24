525c8cbf944ec5ec0fabf5c9885dfab234cbfd2a
krukle <christofferfareseid@gmail.com>
2023-02-24 13:12:50 +0100

    update data sent to numbers

--- micropython_serial_io/uart.py
+++ micropython_serial_io/uart.py
@@ -1,12 +1,14 @@
 from machine import UART
+import random
 import time
 
 uart = UART(2, 15200, bits=8, parity=None, stop=1)
 uart.init(15200, bits=8, parity=None, stop=1)
 
 while True:
-    print("Sending data")
-    uart.write("Hello World")
+    data = random.randint(0, 255)
+    print(f"Sending data: {data}")
+    uart.write(data.to_bytes(1, "big"))
     received = uart.read()
     if (received):
         print(f"Data received: {received}")
